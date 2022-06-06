4b93b0a9e8081b40e517770d2d6d978223dcd6c7
Miguel Bittencourt <79655356+miguelbittencourt@users.noreply.github.com>
2022-06-06 10:34:46 -0300

    Add files via upload

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,14 @@
+﻿float media;
+
+Console.WriteLine("Entre com a media: ");
+media = float.Parse(Console.ReadLine());
+
+Console.WriteLine("-Checagem else if");
+if (media >= 7)
+    Console.WriteLine("Aluno aprovado!");
+else if (media < 7 && media >= 5)
+    Console.WriteLine("Aluno em recuperação!");
+else Console.WriteLine("Aluno reprovado!");
+
+Console.WriteLine("-Checagem ternário");
+Console.WriteLine(media >= 7 ? "Aprovado" : "Reprovado");
